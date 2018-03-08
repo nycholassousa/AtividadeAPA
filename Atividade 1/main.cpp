8ae30814a8d53a4ccd163ffcb24fb7acba69b712
Nycholas <nycholas@live.com>
2018-03-08 12:39:58 -0300

    Adding Comments

--- Atividade 1/main.cpp
+++ Atividade 1/main.cpp
@@ -5,6 +5,7 @@ void selectionSort(std::vector<int>& vec);
 void insertionSort(std::vector<int>& vec);
 
 int main(int argc, char **argv){
+	// If the parameter size does not match what is expected, the following message is displayed:
 	if (argc != 4){
 		std::cout << "Use: ./programname number file_in file_out" << std::endl;
 		std::cout << "Example: ./program 1 example.in out.txt" << std::endl;
@@ -14,35 +15,51 @@ int main(int argc, char **argv){
         return 1;
 	}
 
+	// value of the vector/array
 	int value;
 	bool firstEntry = true;
 	std::ifstream fileIn (argv[2], std::ios::in); // ./program number example.in
 	std::vector<int> vec;
 
+	// read the file
 	while(fileIn >> value){
+		// firstentry/firstvalue = size of the vector
 		if (firstEntry == true){
 			vec.reserve(value);
 			firstEntry = false;
 		}
+		// insert the other values in the vector
 		else
 			vec.push_back(value);
 	}
 	fileIn.close();
 	
-	switch(atoi(argv[1])){
-		case 1:{
+	switch(atoi(argv[1])){ //convert number string in int
+		case 1:{ //Selection Sort
+
+			std::cout << "Selection Sort selected, please wait a moment" <<std::endl;
+
+			// Start counting Time
 			const auto start_time = std::chrono::steady_clock::now();
 			selectionSort(vec);
 			double time_in_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>
   				(std::chrono::steady_clock::now() - start_time).count();
+			// End counting Time
+
 			std::cout << "Selection Sort Time Elapsed: " << time_in_milliseconds << " milliseconds" <<std::endl;
 			break;
 		}
-		case 2:{
+		case 2:{ //Insertion Sort
+
+			std::cout << "Insertion Sort selected, please wait a moment" <<std::endl;
+
+			// Start counting Time
 			const auto start_time = std::chrono::steady_clock::now();
 			insertionSort(vec);
 			double time_in_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>
   				(std::chrono::steady_clock::now() - start_time).count();
+			// End counting Time
+
 			std::cout << "Insertion Sort Time Elapsed: " << time_in_milliseconds << " milliseconds" <<std::endl;
 			break;
 		}
@@ -51,9 +68,9 @@ int main(int argc, char **argv){
 		}
 	}
 
-  	std::ofstream output_file(argv[3]);
-    std::ostream_iterator<int> output_iterator(output_file, "\n");
-    std::copy(vec.begin(), vec.end(), output_iterator);	
+  	std::ofstream output_file(argv[3]); //create an output file 
+    std::ostream_iterator<int> output_iterator(output_file, "\n"); 
+    std::copy(vec.begin(), vec.end(), output_iterator);	//copy the ordered values of the vector in the file
 
 	return 0;
 }
--- Atividade 1/sorts.h
+++ Atividade 1/sorts.h
@@ -4,23 +4,47 @@
 #include <vector>
 #include <stdlib.h>
 
+// Selection Sort
+/* How it works:
+** The algorithm divides the input list into two parts: the sublist of items already sorted, 
+** which is built up from left to right at the front (left) of the list, and the sublist of 
+** items remaining to be sorted that occupy the rest of the list. Initially, the sorted sublist 
+** is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding 
+** the smallest (or largest, depending on sorting order) element in the unsorted sublist, 
+** exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), 
+** and moving the sublist boundaries one element to the right.
+*/
 void selectionSort(std::vector<int>& vec){
 	int i, j, min;
 	int unsorted;
 
 	for (i = 0; i < vec.size() - 1; i++){
+		// choose the first value
 		min = i;
 		for (j = i + 1; j < vec.size(); j++)
+			//if the next value is smaller than the previous value
 			if (vec[j] < vec[min])
+				//choose new position
 				min = j;
+		//swap values
 		std::swap(vec[min], vec[i]);
 	}
 }
 
+// Insertion Sort
+/* How it works:
+** Insertion sort iterates, consuming one input element each repetition, 
+** and growing a sorted output list. At each iteration, insertion sort removes one element from the input data, 
+** finds the location it belongs within the sorted list, and inserts it there. 
+** It repeats until no input elements remain.
+*/
 void insertionSort(std::vector<int>& vec){
 	int i, j, unsorted;
 	for (j = 1; j < vec.size(); ++j){
+		//starting with second value of the vector
 		unsorted = vec[j];
+		//compare the previous value if it is not in the first position
+		//and if the non-ordered value is less than the same
 		for (i = j - 1; i >= 0 && vec[i] > unsorted; --i)
 			vec[i + 1] = vec[i];
 		vec[i + 1] =  unsorted;
